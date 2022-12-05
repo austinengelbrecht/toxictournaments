00d08896bf199dce8b987da687cbf576294c2bf5
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-04 21:10:46 -0600

    Init | Allowing scroll behaviror 📜

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
@@ -10,6 +10,18 @@ const router = createRouter({
       component: HomeView,
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    } else if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
