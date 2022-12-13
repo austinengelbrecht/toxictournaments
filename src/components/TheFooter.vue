2bebe697c2571cf616c976d4ace40252e93354e7
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-12 22:52:56 -0600

    Code clean up

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,18 +4,51 @@
       <ToxicLogo class="footer-home-logo" />
       <h2 class="footer-home-title">Toxic Tournaments</h2>
     </router-link>
+
+    <ul>
+      <li v-for="link in socialLinks">
+        <a :key="link.url" :href="link.url" target="_blank">
+          <Icon :icon="link.icon" class="icon" />
+          <span class="sr-only">{{ link.linkText }}</span>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import ToxicLogo from "@/assets/icons/ToxicLogo.vue";
+import { Icon } from "@iconify/vue";
 
 export default {
   components: {
     ToxicLogo,
   },
   data() {
-    return {};
+    return {
+      socialLinks: [
+        {
+          url: "#",
+          linkText: "Goto our YouTube",
+          icon: "fa:github",
+        },
+        {
+          url: "#",
+          linkText: "Goto our Twitter",
+          icon: "fa:linkedin",
+        },
+        {
+          url: "#",
+          linkText: "Goto our Twitter",
+          icon: "fa:linkedin",
+        },
+        {
+          url: "#",
+          linkText: "Goto our Twitter",
+          icon: "fa:linkedin",
+        },
+      ],
+    };
   },
 };
 </script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,51 +4,18 @@
       <ToxicLogo class="navbar-home-logo" />
       <h1>Toxic Tournaments</h1>
     </router-link>
-
-    <ul>
-      <li v-for="link in socialLinks">
-        <a :key="link.url" :href="link.url" target="_blank">
-          <Icon :icon="link.icon" class="icon" />
-          <span class="sr-only">{{ link.linkText }}</span>
-        </a>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
 import ToxicLogo from "@/assets/icons/ToxicLogo.vue";
-import { Icon } from "@iconify/vue";
 
 export default {
   components: {
     ToxicLogo,
   },
   data() {
-    return {
-      socialLinks: [
-        {
-          url: "#",
-          linkText: "Goto our YouTube",
-          icon: "fa:github",
-        },
-        {
-          url: "#",
-          linkText: "Goto our Twitter",
-          icon: "fa:linkedin",
-        },
-        {
-          url: "#",
-          linkText: "Goto our Twitter",
-          icon: "fa:linkedin",
-        },
-        {
-          url: "#",
-          linkText: "Goto our Twitter",
-          icon: "fa:linkedin",
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
