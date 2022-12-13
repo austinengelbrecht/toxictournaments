2200709527a4a2bf2ea49a2ab767d095dd07eef0
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-12 20:55:13 -0600

    Nav | Navigation Setup 🧭

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Test Header</h1>
+  <div class="nav bg-onyx-3">
+    <ToxicLogo class="nav-logo" />
+  </div>
 </template>
+
+<script>
+import ToxicLogo from "@/assets/icons/ToxicLogo.vue";
+
+export default {
+  components: {
+    ToxicLogo,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  &-logo {
+    height: 100px;
+    width: auto;
+  }
+}
+</style>
