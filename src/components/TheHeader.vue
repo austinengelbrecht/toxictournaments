383b780c46a72f1d05d19556cf202ac9243f9337
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-12 22:49:40 -0600

    Code cleanup and font fixing

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,20 +6,49 @@
     </router-link>
 
     <ul>
-      <li></li>
+      <li v-for="link in socialLinks">
+        <a :key="link.url" :href="link.url" target="_blank">
+          <Icon :icon="link.icon" class="icon" />
+          <span class="sr-only">{{ link.linkText }}</span>
+        </a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 import ToxicLogo from "@/assets/icons/ToxicLogo.vue";
+import { Icon } from "@iconify/vue";
 
 export default {
   components: {
     ToxicLogo,
   },
   data() {
-    return {};
+    return {
+      socialLinks: [
+        {
+          url: "#",
+          linkText: "Goto our YouTube",
+          icon: "fa:github",
+        },
+        {
+          url: "#",
+          linkText: "Goto our Twitter",
+          icon: "fa:linkedin",
+        },
+        {
+          url: "#",
+          linkText: "Goto our Twitter",
+          icon: "fa:linkedin",
+        },
+        {
+          url: "#",
+          linkText: "Goto our Twitter",
+          icon: "fa:linkedin",
+        },
+      ],
+    };
   },
 };
 </script>
