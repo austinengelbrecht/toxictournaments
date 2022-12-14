151229165ea16efa30ee36b071e0a7237adc610a
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-13 22:57:27 -0600

    Trailer | Refactoring and styling

--- src/components/TheTrailer/IntroTrailer.vue
+++ src/components/TheTrailer/IntroTrailer.vue
@@ -1,20 +1,20 @@
 <template>
-  <trailer-background>
+  <section class="trailer bg-onyx-2">
     <YoutubeTrailer />
 
-    <div>
+    <div class="trailer-text">
       <h2>Are We Toxic?</h2>
+      <p>Add some text here or something</p>
+      <a href="#">Then probably a button here</a>
     </div>
-  </trailer-background>
+  </section>
 </template>
 
 <script>
-import TrailerBackground from "./TrailerBackground.vue";
 import YoutubeTrailer from "./YoutubeTrailer.vue";
 
 export default {
   components: {
-    TrailerBackground,
     YoutubeTrailer,
   },
   data() {
@@ -22,3 +22,18 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.trailer {
+  display: flex;
+  flex-flow: row wrap;
+  padding: 1rem;
+  margin: 2rem 0;
+  // background: linear-gradient(-30deg, #3d0959, #2a2a2a 25% 75%, #3d0959);
+
+  &-text {
+    display: flex;
+    flex-flow: column nowrap;
+  }
+}
+</style>
