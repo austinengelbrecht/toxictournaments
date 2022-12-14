2805ea4dd5f3203b33b219614f400c741a933912
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-13 22:51:32 -0600

    Global | Applied reset to html

--- src/components/TheTrailer/TrailerBackground.vue
+++ src/components/TheTrailer/TrailerBackground.vue
@@ -8,8 +8,7 @@
 .background {
   padding: 1rem;
   margin: 2rem 0;
-  width: 100%;
 
-  background: linear-gradient(90deg, #3d0959, #2a2a2a 30% 70%, #3d0959);
+  background: linear-gradient(-30deg, #3d0959, #2a2a2a 25% 75%, #3d0959);
 }
 </style>
