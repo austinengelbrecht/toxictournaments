16b4c4799076a4511889c3142d20c3a58c158643
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-21 17:48:16 -0600

    Adding colors on hover

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :to="goto" class="button">
+  <router-link :to="goto" class="button glow-on-hover">
     <div class="button-text">{{ text }}</div>
   </router-link>
 </template>
@@ -24,16 +24,93 @@ export default {
 
   background-color: $black;
 
-  &:hover {
-    background-color: $onyx-2;
-    border-color: $poison-1;
-    transition: 0.5s ease-in-out;
-  }
+  // &:hover {
+  //   background-color: $onyx-2;
+  //   border-color: $poison-1;
+  //   transition: 0.5s ease-in-out;
+  // }
 
   &-text {
     text-align: center;
     margin: auto;
     font-size: 1.15rem;
+  }
+}
+
+.glow-on-hover {
+  width: 220px;
+  height: 50px;
+  border: none;
+  outline: none;
+  color: #fff;
+  background: #111;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
+  border-radius: 10px;
+}
+
+.glow-on-hover:before {
+  content: "";
+  background: linear-gradient(
+    45deg,
+    #ff0000,
+    #ff7300,
+    #fffb00,
+    #48ff00,
+    #00ffd5,
+    #002bff,
+    #7a00ff,
+    #ff00c8,
+    #ff0000
+  );
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  background-size: 400%;
+  z-index: -1;
+  filter: blur(5px);
+  width: calc(100% + 4px);
+  height: calc(100% + 4px);
+  animation: glowing 20s linear infinite;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+  border-radius: 10px;
+}
+
+.glow-on-hover:active {
+  color: #000;
+}
+
+.glow-on-hover:active:after {
+  background: transparent;
+}
+
+.glow-on-hover:hover:before {
+  opacity: 1;
+}
+
+.glow-on-hover:after {
+  z-index: -1;
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #111;
+  left: 0;
+  top: 0;
+  border-radius: 10px;
+}
+
+@keyframes glowing {
+  0% {
+    background-position: 0 0;
+  }
+  50% {
+    background-position: 400% 0;
+  }
+  100% {
+    background-position: 0 0;
   }
 }
 </style>
