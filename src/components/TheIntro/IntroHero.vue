248ec6380016325c23887ab44e3fe78f44bdedc8
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-21 22:25:51 -0600

    Hero | Test styling

--- src/components/TheIntro/IntroHero.vue
+++ src/components/TheIntro/IntroHero.vue
@@ -1,11 +1,20 @@
 <template>
   <section class="hero">
-    <div></div>
+    <div class="intro">
+      <ToxicLogo class="intro-logo" />
+      <h1 class="intro-heading">Toxic Tournaments</h1>
+      <p class="intro-tagline">
+        Welcome To The Best Mid-West Smash Bros Tournament Hosting
+      </p>
+    </div>
   </section>
 </template>
 
 <script>
+import ToxicLogo from "../../assets/icons/ToxicLogo.vue";
+
 export default {
+  components: { ToxicLogo },
   data() {
     return {};
   },
@@ -16,5 +25,31 @@ export default {
 .hero {
   background-image: url(../../assets/images/intro_background.png);
   min-height: 392px;
+}
+
+.intro {
+  display: flex;
+  flex-flow: column nowrap;
+  max-width: fit-content;
+  padding: 1.5rem;
+  border-radius: 10px;
+  margin: 0 auto;
+  //background-color: rgb(58, 58, 58);
+
+  &-logo {
+    max-height: 200px;
+    width: auto;
+  }
+
+  &-heading {
+    margin: 0;
+    text-align: center;
+    font-size: 3rem;
+  }
+
+  &-tagline {
+    text-align: center;
+    font-size: 1.25rem;
+  }
 }
 </style>
