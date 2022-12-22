cb11a88557640b8f99a07dab23e0e6cbffaf65d6
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-12-21 21:40:23 -0600

    Introduction Header setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import IntroHeader from "../components/TheIntro/IntroHeader.vue";
 import IntroTrailer from "../components/TheTrailer/IntroTrailer.vue";
 </script>
 
 <template>
   <main>
+    <IntroHeader />
     <IntroTrailer />
   </main>
 </template>
