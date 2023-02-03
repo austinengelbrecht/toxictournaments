a9679f66a8aede38e3d0a699d91f03685ee88c1c
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-02 21:04:38 -0600

    Moving files and building new hero

--- src/components/TheIntro/NewIntroHero.vue
+++ src/components/TheIntro/NewIntroHero.vue
@@ -1,9 +1,21 @@
 <template>
-  <section class="hero"></section>
+  <section class="hero">
+    <div>
+      <h1>Bring your competition to life</h1>
+      <p>
+        Elevate your gaming skills, build your competitive spirit, and
+        experience the excitement as you climb the tournament brackets.
+      </p>
+    </div>
+
+    <YoutubeTrailer />
+  </section>
 </template>
 
 <script>
-export default {};
+import YoutubeTrailer from "./YoutubeTrailer.vue";
+
+export default { components: { YoutubeTrailer } };
 </script>
 
 <style lang="scss" scoped></style>
