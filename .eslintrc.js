fad1fdeaf9b58ca76de74e4467737bb2d1adb16f
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-09 21:15:58 -0600

    Adding Vue Linter and configureing setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,11 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
+    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-strongly-recommended",
   ],
   rules: {
     // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    // 'vue/no-unused-vars': 'error' 
   }
 }
