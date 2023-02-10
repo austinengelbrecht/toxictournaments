80238049b88e60bd1868512e9b485897d1315cb9
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-09 21:50:40 -0600

    Linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:vue/vue3-recommended",
     "plugin:vue/vue3-essential",
-    "plugin:vue/vue3-strongly-recommended",
     "prettier"
   ],
   rules: {
