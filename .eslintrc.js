34770108cfd7f9fb0f6e13ef8fcb08b7ee1a6891
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-09 21:18:20 -0600

    Fix - Conficts between prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     "plugin:vue/vue3-essential",
     "plugin:vue/vue3-strongly-recommended",
+    "prettier"
   ],
   rules: {
     // override/add rules settings here, such as:
