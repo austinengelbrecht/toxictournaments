2ca6188042a4902fc9733a7b6f45236a8c6502da
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-09 21:00:20 -0600

    Refactoring site strcuture

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import TheComingSoon from "../components/TheComingSoon.vue";
-import IntroHero from "../components/IntroTailer/IntroHero.vue";
+import IntroHero from "../components/IntroHero/IntroHero.vue";
 </script>
 
 <template>
