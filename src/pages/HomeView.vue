e97a729688632ebb1efea9bba3c89f396ea2b812
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-10 20:10:26 -0600

    Creation of TailwindCSS test file

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheComingSoon from "../components/TheComingSoon.vue";
 import IntroHero from "../components/IntroHero/IntroHero.vue";
+import TheTailwindTest from "../components/TheTailwindTest.vue";
 </script>
 
 <template>
   <main>
     <IntroHero />
     <TheComingSoon />
+    <TheTailwindTest />
   </main>
 </template>
