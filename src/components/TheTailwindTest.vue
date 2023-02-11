a88e12567d4691a5c6b0f4f057f2feb6f3e40194
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-10 20:17:36 -0600

    Successful setup of TailwindCSS

--- src/components/TheTailwindTest.vue
+++ src/components/TheTailwindTest.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <section class="">
+    <h2 class="text-5xl text-toxic-1">Hey I'm testing tailwindCSS</h2>
+  </section>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "wt": "#ffffff",
+      "toxic-1": "#13c900",
+    },
     extend: {},
   },
   plugins: [],
