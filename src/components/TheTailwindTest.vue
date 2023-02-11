866406aee61b4c36eed272a36592cee972ae652d
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-10 21:21:16 -0600

    TailwindCSS Config - Colors 🎨

--- src/components/TheTailwindTest.vue
+++ src/components/TheTailwindTest.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="">
-    <h2 class="text-5xl text-toxic-1">Hey I'm testing tailwindCSS</h2>
+  <section class="bg-midnight padding">
+    <h2 class="text-8xl text-toxic-1">Hey I'm testing tailwindCSS</h2>
   </section>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,31 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      "wt": "#ffffff",
-      "toxic-1": "#13c900",
+      transparent: "transparent",
+      current: "current",
+      white: "#ffffff",
+      offwhite: "#ebebeb",
+      black: "#000000",
+
+      logo: {
+        1: "#14d900",
+        2: "#13c900",
+        3: "#08a700",
+      },
+      toxic: {
+        1: "#1aff00",
+        2: "#18f200",
+        3: "#13c900",
+        4: "#13bf00",
+        5: "#11a600",
+      },
+      poison: {
+        1: "#8313bf",
+        2: "#7211a6",
+        3: "#600e8c",
+        4: "#4f0b73",
+        5: "#3d0959",
+      },
     },
     extend: {},
   },
