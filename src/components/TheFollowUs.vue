aabc9506ada388d205b85c3b01bfe686bf5ab04b
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-12 13:31:25 -0600

    Creation and registration of follow us component

---
+++ src/components/TheFollowUs.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h2 class="capitalize">Follow Us</h2>
+    <ul>
+      <li>
+        <Icon />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import socialLinks from "@/assets/data/social_links.vue";
+import { Icon } from "@iconify/vue";
+
+export default {
+  data() {
+    return {
+      socialLinks: socialLinks,
+    };
+  },
+  components: { Icon },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
 import TheComingSoon from "../components/TheComingSoon.vue";
 import IntroHero from "../components/IntroHero/IntroHero.vue";
+import TheFollowUs from "../components/TheFollowUs.vue";
 </script>
 
 <template>
   <main>
     <IntroHero />
+    <TheFollowUs />
     <TheComingSoon />
-    <TheTailwindTest />
   </main>
 </template>
