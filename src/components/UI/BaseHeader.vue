48d5c2e9e26f6c4c3d8b2e9a9a3bb84db38b5ca9
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-13 20:21:33 -0600

    Init of Section Header component

---
+++ src/components/UI/BaseHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String },
+    subtitle: { type: String },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
