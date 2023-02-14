1db1316a16bdd3822cfb9b9410d837085e390e4c
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-13 21:32:24 -0600

    Testing video ideas

--- src/components/LatestVideos/TheLatestVideos.vue
+++ src/components/LatestVideos/TheLatestVideos.vue
@@ -1,6 +1,16 @@
 <template>
-  <section>
+  <section class="mt-20 flex flex-column justify-center">
     <BaseHeader :title="`Watch Our Latest Content`" />
+
+    <ul class="featured-videos">
+      <li
+        class="featured-videos-clip"
+        v-for="video in videoLinks"
+        :key="video.url"
+      >
+        <LatestVideo :video="video.url" />
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -8,9 +18,10 @@
 import featuredVideos from "@/assets/data/featured_videos.json";
 
 import BaseHeader from "../UI/BaseHeader.vue";
+import LatestVideo from "./LatestVideo.vue";
 
 export default {
-  components: { BaseHeader },
+  components: { BaseHeader, LatestVideo },
   data() {
     return {
       videoLinks: featuredVideos,
@@ -19,4 +30,18 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.featured-videos {
+  &-clip {
+    &:nth-child(1) {
+      width: 600px;
+      height: 350px;
+    }
+
+    &:nth-child(n + 2) {
+      width: 250px;
+      height: auto;
+    }
+  }
+}
+</style>
