d172d960c8e47b35a35c419c46bd2c5036eebf6d
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-13 20:57:14 -0600

    Adding featured video data

--- src/components/LatestVideos/TheLatestVideos.vue
+++ src/components/LatestVideos/TheLatestVideos.vue
@@ -5,9 +5,18 @@
 </template>
 
 <script>
+import featuredVideos from "@/assets/data/featured_videos.json";
+
 import BaseHeader from "../UI/BaseHeader.vue";
 
-export default { components: { BaseHeader } };
+export default {
+  components: { BaseHeader },
+  data() {
+    return {
+      videoLinks: featuredVideos,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
