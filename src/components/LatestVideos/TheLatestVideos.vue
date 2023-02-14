ad8f4206e93ef0950e19decb8b4ba597e9c836e7
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-13 20:38:25 -0600

    Registering new section on Homepage

---
+++ src/components/LatestVideos/TheLatestVideos.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <BaseHeader :title="`Our Latest Content`" />
+  </section>
+</template>
+
+<script>
+import BaseHeader from "../UI/BaseHeader.vue";
+
+export default { components: { BaseHeader } };
+</script>
+
+<style lang="scss" scoped></style>
