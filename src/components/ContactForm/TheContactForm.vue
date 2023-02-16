260889095e70b672bbb5442f21ace669900033a9
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-15 21:58:18 -0600

    Creation and Registration of contact form

---
+++ src/components/ContactForm/TheContactForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <BaseHeader :title="`Sign Up`" />
+
+    <ContactForm />
+  </section>
+</template>
+
+<script>
+import BaseHeader from "../UI/BaseHeader.vue";
+import ContactForm from "./ContactForm.vue";
+
+export default { components: { BaseHeader, ContactForm } };
+</script>
+
+<style lang="scss" scoped></style>
