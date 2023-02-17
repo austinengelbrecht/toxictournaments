a47c911a6807f0cc489324a873aa0821b9877f12
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-16 19:13:32 -0600

    Testing axios setup

--- src/components/ContactForm/ContactForm.vue
+++ src/components/ContactForm/ContactForm.vue
@@ -4,7 +4,12 @@
       <label for="newsletter-signup" class="capitalize"
         >Enter Email Address</label
       >
-      <input type="email" name="News Letter Signup" id="newsletter-signup" />
+      <input
+        type="email"
+        name="News Letter Signup"
+        id="newsletter-signup"
+        v-model="userEmail"
+      />
     </div>
 
     <button type="submit">Sign Up</button>
@@ -12,7 +17,17 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      userEmail: "",
+      isSubmitted: false,
+    };
+  },
+  methods: {
+    submitEmail() {},
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
