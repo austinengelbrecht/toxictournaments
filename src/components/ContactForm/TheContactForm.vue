cbc12e1aeb5b094d3653a30fd0952785909a5e05
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-18 23:00:03 -0600

    Testing icon

--- src/components/ContactForm/TheContactForm.vue
+++ src/components/ContactForm/TheContactForm.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="signup" class="px-8 py-16 bg-onyx-2 mt-[10rem]">
     <div class="border-solid border-toxic-3 border-4 p-8">
-      <ClassicController class="w-[100px] h-[100px]" />
+      <ClassicController class="w-[300px] h-[300px] bg-black" />
 
       <div class="mb-8">
         <h2 class="text-6xl">Want to game with us?</h2>
