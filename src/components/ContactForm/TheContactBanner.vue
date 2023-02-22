91791bfd4176b966686e34932ed0c1ae2a4c212a
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-02-21 21:17:37 -0600

    Styling and modal prep

--- src/components/ContactForm/TheContactBanner.vue
+++ src/components/ContactForm/TheContactBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <section
     id="signup"
-    class="flex flex-row flex-wrap justify-center content-center mt-[10rem] bg-onyx-1 max-w-[1000px] p-8 mx-auto my-0 rounded-2xl"
+    class="flex flex-row flex-wrap justify-center gap-8 content-center mt-[10rem] bg-onyx-1 max-w-[1000px] p-8 mx-auto my-0 rounded-2xl"
   >
     <ClassicController class="w-[250px] h-[250px]" />
 
@@ -15,7 +15,7 @@
       </p>
 
       <button
-        class="glow-on-hover capitalize py-4 px-8 max-w-[200px] text-center"
+        class="glow-on-hover capitalize py-4 px-8 max-w-[200px] text-center self-end"
       >
         Join the toxic Community
       </button>
@@ -26,7 +26,12 @@
 <script>
 import ClassicController from "@/assets/icons/classiccontroller.vue";
 
-export default { components: { ClassicController } };
+export default {
+  components: { ClassicController },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped>
