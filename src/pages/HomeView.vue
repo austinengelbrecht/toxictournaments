ccb4cca71a7419c61bb49641b52a942020926193
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-03-13 18:10:07 -0500

    Improving performance on initial page load

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,15 +1,23 @@
 <script setup>
-import ContactBanner from "../components/ContactForm/ContactBanner.vue";
+//import ContactBanner from "../components/ContactForm/ContactBanner.vue";
 import IntroHero from "../components/IntroHero/IntroHero.vue";
-import TheLatestVideos from "../components/LatestVideos/TheLatestVideos.vue";
+// import TheLatestVideos from "../components/LatestVideos/TheLatestVideos.vue";
 import TheFollowUs from "../components/TheFollowUs.vue";
+
+import { defineAsyncComponent } from "vue";
+
+const TheLatestVideos = defineAsyncComponent(() =>
+  import("../components/LatestVideos/TheLatestVideos.vue")
+);
+
+const ContactBanner = defineAsyncComponent(() =>
+  import("../components/ContactForm/ContactBanner.vue")
+);
 </script>
 
 <template>
-  <main>
-    <IntroHero />
-    <TheFollowUs />
-    <TheLatestVideos />
-    <ContactBanner />
-  </main>
+  <IntroHero />
+  <TheFollowUs />
+  <TheLatestVideos />
+  <ContactBanner />
 </template>
