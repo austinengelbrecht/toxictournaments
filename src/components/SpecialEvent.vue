2db58f174177b1828ffa7f0f5796118890483f41
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-11-07 20:08:09 -0600

    Add in event panner

--- src/components/SpecialEvent.vue
+++ src/components/SpecialEvent.vue
@@ -1,10 +1,25 @@
 <template>
-  <section class="flex mx-auto my-0">
-    Logo Here
+  <section
+    class="flex mx-auto my-0 gap-4 items-center justify-center mt-12 p-4 bg-onyx-1"
+  >
+    <img
+      src="/OutbreakLogo.png"
+      alt="Outbreak 2023 Logo"
+      loading="lazy"
+      aria-hidden="true"
+      height="100"
+      width="100"
+    />
 
-    <div class="flex flex-col">
-      Join the Outbreak Event!
-      <a href="" class="button">Sign Up Here</a>
+    <div class="flex flex-col gap-4">
+      We are currently hosting a special event! Join the Outbreak Event!
+      <a
+        href="http://34.71.9.100/"
+        rel="nofollow"
+        target="_blank"
+        class="button text-center self-start"
+        >Learn More</a
+      >
     </div>
   </section>
 </template>
@@ -16,6 +31,15 @@ export default {};
 <style lang="scss" scoped>
 .button {
   padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
+  border: black 4px solid;
+  border-radius: 0.25rem;
+  background-color: #e21313;
+  color: black;
+  transition: background-color 0.3s ease-in-out;
+
+  &:hover,
+  &:focus {
+    background-color: $toxic-3;
+  }
 }
 </style>
