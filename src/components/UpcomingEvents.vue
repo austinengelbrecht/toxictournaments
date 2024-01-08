b7a7c4d06d45f81165ddff6ea0acdf567eeb0411
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2024-01-07 20:55:36 -0600

    Add prep for loading

--- src/components/UpcomingEvents.vue
+++ src/components/UpcomingEvents.vue
@@ -1,13 +1,46 @@
 <template>
   <section>
-    <h2>Upcoming Events</h2>
+    <BaseHeader :title="`Upcoming Events`" />
+
+    <div v-if="loading">Loading....</div>
+
+    <div v-else-if="error">Error</div>
+
+    <div v-else>Got Data</div>
   </section>
 </template>
 
 <script>
+import BaseHeader from "./UI/baseheader.vue";
+import axios from "axios";
+
 export default {
-  setup() {
-    return {};
+  components: { BaseHeader },
+  data() {
+    return {
+      loading: true,
+      error: false,
+      data: null,
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      const apiUrl = "https://catfact.ninja/fact";
+
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          this.data = response.data;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error("Error getting data", error);
+          this.error = true;
+        });
+    },
   },
 };
 </script>
